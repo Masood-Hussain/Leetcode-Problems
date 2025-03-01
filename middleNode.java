636566de7c243945e14bf1cd19b5bafcbdbb6732
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-01 19:17:38 +0500

    middleNode

---
+++ middleNode.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+    int count = 0;
+            ListNode p = head;
+    
+            while (p != null) {
+                count++;
+                p = p.next;
+            }
+    
+            int key = (count / 2) + 1;
+    
+            p = head;
+            for (int i = 1; i < key; i++) {
+                p = p.next;
+            }
+    
+            return p;
+        } 
+    }
