d54fde9e90039d69589fe947966f3c74fed46133
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-29 06:06:21 -0800

    deleteDuplicates

---
+++ deleteDuplicates.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+            if (head == null) {
+            return null;
+        }
+        
+        ListNode current = head;
+        
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next; // Skip the duplicate node
+            } else {
+                current = current.next; // Move to the next distinct element
+            }
+        }
+        
+        return head;
+    }
+}
