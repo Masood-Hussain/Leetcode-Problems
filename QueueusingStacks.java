fd82b265f29d5e2d8d98655f84f5b02aadc87725
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-04-17 20:23:08 -0800

    MyQueue

---
+++ QueueusingStacks.java
@@ -0,0 +1,35 @@
+class MyQueue {
+    private Stack<Integer> pushStack;
+    private Stack<Integer> popStack;
+
+    public MyQueue() {
+        pushStack = new Stack<>();
+        popStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        pushStack.push(x);
+    }
+    
+    public int pop() {
+        if (popStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.pop();
+    }
+    
+    public int peek() {
+        if (popStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.peek();
+    }
+    
+    public boolean empty() {
+        return pushStack.isEmpty() && popStack.isEmpty();
+    }
+}
